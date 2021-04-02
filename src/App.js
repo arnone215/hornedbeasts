5f510204af25bd1637697196d28ff6c686ce2258
arnone215 <tarnone21589@gmail.com>
2021-04-01 21:02:25 -0700

    lab3 finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-
 import React from 'react';
+import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
+import Footer from './Footer.js'
+import SelectedBeast from './SelectedBeast';
+
 import data from './data.json';
-import Footer from './Footer.js';
 
 class App extends React.Component {
-    constructor(props){
-    super()
-    this.state={
-      
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      beast: {}
     }
   }
+
+  showBeastInModal = (clickedBeast) => {
+    this.setState({
+      show: true,
+      beast: clickedBeast,
+      
+    }) 
+    console.log(clickedBeast);
+  }
+
+  hideBeastInModal = () => {
+    this.setState({
+      show: false,
+    })
+  }
+
+
   render() {
-    return(
+    console.log(this.state)
+    return (
       <div>
         <Header />
-        <Main info={data} />
+        <Main 
+          beasts={data}
+          handleClick={this.showBeastInModal}
+        />
+        <SelectedBeast
+        show={this.state.show}
+        hideBeast={this.hideBeastInModal}
+        beast={this.state.beast}
+        />
+        
         <Footer />
       </div>
     )
   }
 }
+
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,30 @@
-import React from 'react';
+import React from 'react'; 
 import HornedBeast from './HornedBeast';
-import CardColumns from 'react-bootstrap/CardColumns';
-import './Main.css'
-
-
+import CardColumns from 'react-bootstrap/CardColumns'
+import './Main.css';
 
 
 class Main extends React.Component {
-  // constructor(props){
-  //   super()
-  //   this.state={
-  //     data: this.props.info
-  //   }
-  // }
-  render() {
-    console.log(this.props.info)
-    let beastArray = this.props.info.map ( animal => {
-      return <HornedBeast
-        key= {animal.title}
-        name = {animal.keyword}
-        title = {animal.title}
-        image_url = {animal.image_url}
-        description = {animal.description}
-        />
-    });
+    render () {
+        let beastArray = this.props.beasts.map((beast, index) => (
+            <HornedBeast
+            name = {beast.keyword}
+            title = {beast.ttle}
+            image_url = {beast.image_url}
+            description = {beast.description}
+            key={index}
+            handleClick={this.props.handleClick}
+            />
+        ));
 
-    return (
-        <CardColumns>
-        {beastArray}
-        </CardColumns>
-    );
-  }
+            return (
+            <main>
+             <CardColumns>
+                {beastArray} 
+             </CardColumns>
+            </main>
+        );
+    }
 }
 
 export default Main;
